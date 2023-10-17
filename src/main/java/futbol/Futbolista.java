9edd6b36519ba79ef925d8e0a11fdaa78667a6c4
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-17 10:53:21 -0500

    Update Futbolista.java

--- src/main/java/futbol/Futbolista.java
+++ src/main/java/futbol/Futbolista.java
@@ -11,6 +11,33 @@ public abstract class Futbolista implements Comparable<Object>{
 		this.posicion = posicion;
 	}
 	
+	public Futbolista() {
+		this("Maradona",30,"delantero");
+	}
+	
+	public String toString() {
+		return "El futbolista "+nombre+" tiene "+edad+ ", y juega de "+posicion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
 	
 	
 }
