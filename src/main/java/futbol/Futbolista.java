7866bbd97459bbf1274d85e4b25c4f9f9665f0eb
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-17 10:52:49 -0500

    Update Futbolista.java

--- src/main/java/futbol/Futbolista.java
+++ src/main/java/futbol/Futbolista.java
@@ -1,4 +1,16 @@
+package futbol;
 
-public class Futbolista {
-
+public abstract class Futbolista implements Comparable<Object>{
+	private String nombre;
+	private int edad;
+	private final String posicion;
+	
+	public Futbolista(String nombre, int edad, String posicion) {
+		this.nombre = nombre;
+		this.edad = edad;
+		this.posicion = posicion;
+	}
+	
+	
+	
 }
