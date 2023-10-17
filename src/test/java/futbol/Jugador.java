a1cb2e57a25cce00223131028545ab7029dfba4b
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-17 11:01:00 -0500

    Update Jugador.java

--- src/test/java/futbol/Jugador.java
+++ src/test/java/futbol/Jugador.java
@@ -5,6 +5,12 @@ public class Jugador extends Futbolista{
 	public short golesMarcados;
 	public byte dorsal;
 	
+	public Jugador() {
+		super();
+		this.golesMarcados=(short) 289;
+		this.dorsal=(byte) 7;
+	}
+	
 	
 	
 }
