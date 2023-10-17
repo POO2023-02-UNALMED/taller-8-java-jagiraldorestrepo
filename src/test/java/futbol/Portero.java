a9450dbcae9f663f729eb8cd63a9a0b849e572ce
jagiraldorestrepo <ja.giraldorestrepo@gmail.com>
2023-10-17 11:03:08 -0500

    Update Portero.java

--- src/test/java/futbol/Portero.java
+++ src/test/java/futbol/Portero.java
@@ -4,5 +4,15 @@ public class Portero extends Futbolista{
 	public short golesRecibidos;
 	public byte dorsal;
 	
+	public Portero() {
+		
+	}
+	public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+		super(nombre, edad, "Portero");
+		this.golesRecibidos=golesRecibidos;
+		this.dorsal=dorsal;
+	}
+ 
+	
 
 }
